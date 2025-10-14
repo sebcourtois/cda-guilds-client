842cc5b66f9295fecbbaaf0641a6d80f34c14b0b
sebcourtois <neitsabes974@gmail.com>
2025-10-14 16:12:59 +0200

    create spring boot project

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.afpa.chatellerault"
+version = "0.0.1-SNAPSHOT"
+description = "cda-guilds-client"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springShellVersion"] = "3.4.1"
+
+dependencies {
+    implementation("org.springframework.shell:spring-shell-starter")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.shell:spring-shell-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.shell:spring-shell-dependencies:${property("springShellVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/org/afpa/chatellerault/cdaguildsclient/CdaGuildsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afpa.chatellerault.cdaguildsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdaGuildsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
